c7a59f36c82131fbe0a94c40e63f98b1e776b492
sergey_timonin <timonin-93@mail.ru>
2022-10-18 14:08:13 +0300

    Added Base

---
+++ Example_006Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 7;
+int c = 4;
+int d = 9;
+int e = 2;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
